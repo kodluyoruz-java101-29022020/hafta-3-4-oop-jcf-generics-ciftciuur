a7d2d8d35599b1411983ff98eb7a2258c03601e1
ugur ciftci <ugurprog@gmail.com>
2020-03-30 23:17:05 +0300

    index ve eleman yapısı ıcın yapıcı eklendi

--- src/org/kodluyoruz/questionTwo/QNode.java
+++ src/org/kodluyoruz/questionTwo/QNode.java
@@ -5,6 +5,7 @@ public class QNode<E> {
     E element;
     QNode next;
     QNode prev;
+    int index;
 
     public QNode(E element) {
         this.element = element;
@@ -12,7 +13,17 @@ public class QNode<E> {
         this.prev = null;
     }
 
-    public QNode() {
+    public QNode(int index, E element) {
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+        this.index = index;
+    }
 
+    public QNode() {
+        this.element = null;
+        this.next = null;
+        this.prev = null;
+        this.index = 0;
     }
 }
