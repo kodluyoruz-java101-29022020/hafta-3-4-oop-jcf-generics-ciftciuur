4a3d8a9ea5c2df9282fb8d596470d0c89743b688
ugur ciftci <ugurprog@gmail.com>
2020-04-12 22:41:36 +0300

    oop ödevi için küçük bir login
    menusu eklendi

--- src/org/kodluyoruz/quesitonOne/Main.java
+++ src/org/kodluyoruz/quesitonOne/Main.java
@@ -10,13 +10,30 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        menu();
 
     }
 
     private static void menu() {
+        byte menuRequest;
 
         System.out.println("Sigorta Uygulamasına hoş geldiniz");
+        System.out.println("1 - Oturum Aç");
+        System.out.print("İşleminiz :");
+        menuRequest = scanner.nextByte();
+        switch (menuRequest) {
+            case 1:
+                login();
+                break;
+            default:
+                System.out.println("Geçersiz istek , menüde bulunan kısayol tuslarından bırını tercih ediniz");
+                menu();
 
+        }
+
+    }
+
+    private static void login() {
+        //login user
     }
 }
